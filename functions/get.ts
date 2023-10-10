27fd97b6c522d30a62f98e6937b94c6b7472c4bd
Cole Bosmann <cole@netlify.com>
2023-10-10 12:36:49 -0700

    env vars

--- functions/get.ts
+++ functions/get.ts
@@ -2,6 +2,7 @@ import type { Handler, HandlerEvent, HandlerContext } from "@netlify/functions";
 import { Blobs } from "@netlify/blobs";
 
 const handler: Handler = async (event: HandlerEvent, context: HandlerContext) => {
+	console.log("ENVIRONMENT VARIABLES\n" + JSON.stringify(process.env, null, 2))
 	fetch("www.netlify.com")
 	const rawData = Buffer.from(context.clientContext.custom.blobs, "base64");
 	const data = JSON.parse(rawData.toString("ascii"));

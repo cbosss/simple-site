8306bc9eda9e9f915e055ee4662c2b14f75a1588
Cole Bosmann <cole@netlify.com>
2023-10-10 11:42:33 -0700

    blob init

--- netlify/functions/get.ts
+++ netlify/functions/get.ts
@@ -1,6 +1,19 @@
 import type { Handler, HandlerEvent, HandlerContext } from "@netlify/functions";
+import { Blobs } from "@netlify/blobs";
 
 const handler: Handler = async (event: HandlerEvent, context: HandlerContext) => {
+	const rawData = Buffer.from(context.clientContext.custom.blobs, "base64");
+	const data = JSON.parse(rawData.toString("ascii"));
+	const blobs = new Blobs({
+		authentication: {
+			contextURL: data.url,
+			token: data.token,
+		},
+		context: `deploy:${event.headers["x-nf-deploy-id"]}`,
+		siteID: event.headers["x-nf-site-id"],
+	});
+
+
 	return {
 		statusCode: 200,
 		body: JSON.stringify({ message: "Hello World" }),
